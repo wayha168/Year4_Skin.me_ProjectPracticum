a5d2cbfd4780b1371795ebf81c7e570870c96b88
wayha168 <vehaseng1122@example.com>
2025-10-16 10:48:26 +0700

    update

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "https://backend.skinme.store/api/v1",
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
